95d5a2157276a3828f67edad93837b2a1ce24b42
Rudy Ranero <jonegezo@gmail.com>
2026-04-17 18:05:24 -0500

    Configure Next.js for GitHub Pages export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/AbigailsCeramics",
+  assetPrefix: "/AbigailsCeramics/",
 };
 
 export default nextConfig;
